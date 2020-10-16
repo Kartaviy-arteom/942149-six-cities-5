d2573809c208ade977efd5bd34b214f3a7a28a6d
Kartaviy-arteom <46100724+Kartaviy-arteom@users.noreply.github.com>
2020-10-16 14:48:28 +0300

    Delete Reviews.js

--- src/mocks/Reviews.js
+++
@@ -1,9 +0,0 @@
-export default {
-    1: {
-        avatarPath: `img/avatar-max.jpg`,
-        name: `Adam Sandler`,
-        rating: 3.2,
-        date: `April 2019`,
-        reviewText: `not bad`,
-    },
-}
