d52b845eb836ecbf1485bb455e6ec0c1ccaa10b9
Kartaviy-arteom <46100724+Kartaviy-arteom@users.noreply.github.com>
2020-12-01 13:35:46 +0300

    Create  login-page.test.js

---
+++ src/components/login-page/login-page.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import {LoginPage} from "./login-page";
+
+const noop = () => {};
+const activeCity = `Paris`;
+const AuthorizationStatus = {
+  AUTH: `AUTH`,
+  NO_AUTH: `NO_AUTH`,
+};
+
+it(`LoginPage component render correctly`, () => {
+  const tree = renderer.create(
+      <LoginPage
+        onSubmit={noop}
+        activeCity={activeCity}
+        authorizationStatus={AuthorizationStatus.NO_AUTH)
+      />
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
