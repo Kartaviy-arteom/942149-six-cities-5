f62d114838ee6a0262e0acbdfd9ad85a24226597
Kartaviy-arteom <46100724+Kartaviy-arteom@users.noreply.github.com>
2020-12-01 10:40:06 +0300

    Update app.jsx

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import MainPage from "../main-page/main-page";
-import PropTypes from "prop-types";
 import {Switch, Route, Router as BrowserRouter} from "react-router-dom";
 import browserHistory from "../../browser-history";
-import {connect} from "react-redux";
 import PrivateRoute from "../private-route/private-route";
 import LoginPage from "../login-page/login-page";
 import FavoritesPage from "../favorites-page/favorites-page";
 import PropertyPage from "../property-page/property-page";
 
-const App = (props) => {
-  const {offers} = props;
-
+const App = () => {
   return (
     <BrowserRouter history={browserHistory}>
       <Switch>
@@ -20,24 +16,16 @@ const App = (props) => {
         </Route>
         <PrivateRoute exact path = "/favorites" render={() => {
           return (
-            <FavoritesPage offers = {offers}/>
+            <FavoritesPage/>
           );
         }}/>
         <Route path="/offer/:id?" exact component={PropertyPage}></Route>
         <Route>
-          <MainPage offers = {offers} />
+          <MainPage/>
         </Route>
       </Switch>
     </BrowserRouter>
   );
 };
 
-App.propTypes = {
-  offers: PropTypes.array.isRequired,
-};
-
-const mapStateToProps = (state) => ({
-  offers: state.DATA.offers,
-});
-
-export default connect(mapStateToProps)(App);
+export default App;
