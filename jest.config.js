5a2206a4df9fc4e8a4d957506a37a0ca7f45fcd4
Kartaviy-arteom <kartaviy_arteom@mail.ru>
2020-12-02 18:59:17 +0300

    jest css

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  verbose: true,
-  moduleNameMapper: {
-    "\\.(css|less|scss|sss|styl)$": `<rootDir>/node_modules/jest-css-modules`,
-  },
+  "moduleNameMapper": {
+    "\\.(css|less|scss|sss|styl)$": `<rootDir>/node_modules/jest-css-modules`
+  }
 };
