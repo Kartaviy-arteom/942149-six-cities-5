1ecf1a90638407cb9555403980cc19a353c36226
Kartaviy-arteom <46100724+Kartaviy-arteom@users.noreply.github.com>
2020-12-02 15:32:25 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "moduleNameMapper": {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
-    "\\.(css|less)$": `<rootDir>/`
+  verbose: true,
+  moduleNameMapper: {
+    "\\.(css|less|scss|sss|styl)$": `<rootDir>/node_modules/jest-css-modules`,
   },
 };
