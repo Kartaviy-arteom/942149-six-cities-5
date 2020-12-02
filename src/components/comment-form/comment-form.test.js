76b9e8863f182674c61b02140cc06c1e752d18de
Kartaviy-arteom <kartaviy_arteom@mail.ru>
2020-12-02 19:12:59 +0300

    правка теста формы

--- src/components/comment-form/comment-form.test.js
+++ src/components/comment-form/comment-form.test.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import CommentForm from './comment-form';
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
+import reducer from "../../store/reducers/root-reducer";
+import {createStore} from "redux";
 
+const store = createStore(reducer);
 const noop = () => {};
 
 it(`Should CommentForm render correctly`, () => {
   const tree = renderer
-    .create(<CommentForm
-      onItemActive={noop}
-      onTextChange={noop}
-      text={``}
-      activeElement={`1-star`}
-      postComment={noop}
-      currentOfferId={2}
-      onActiveChange={noop}
-      changePostStatus={noop}
-      isPosting={false}
-      isError={false}
-      isActive={false}
-      changeErrorStatus={noop}
-      clearTextValue={noop}
-      clearItemActive={noop}
-    />)
+    .create(
+        <Provider store={store}>
+          <BrowserRouter>
+            <CommentForm
+              onItemActive={noop}
+              onTextChange={noop}
+              text={``}
+              activeElement={`1-star`}
+              postComment={noop}
+              currentOfferId={2}
+              onActiveChange={noop}
+              changePostStatus={noop}
+              isPosting={false}
+              isError={false}
+              isActive={false}
+              changeErrorStatus={noop}
+              clearTextValue={noop}
+              clearItemActive={noop}
+            />
+          </BrowserRouter>
+        </Provider>)
     .toJSON();
 
   expect(tree).toMatchSnapshot();
